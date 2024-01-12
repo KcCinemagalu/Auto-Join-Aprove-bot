4eddb9db772b92d5cc279cdcf46dc8b45f232aad
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2024-01-12 20:43:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ app = Client(
 )
 
 gif = [
-    'https://telegra.ph/file/c86705328b34883edac89.jpg'
+    ''
 ]
 
 
@@ -32,7 +32,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Join Channel : @Kc_Films_2024 __**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id,img, "**Hey {user}\n\nYour Request For {chat} Is Accepted KC ✅ Don't Leave This Channel \n\n__Join Channel : @Kc_Films_2024 __**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
